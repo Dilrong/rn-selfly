33dd7a00149d571ed33bf5068106cfb67189936f
hakseong Lee <wet932@naver.com>
2019-01-18 15:04:15 +0900

    create Header
    create Header component

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ListContainer } from './containers';
+import { Header, StatusBar } from './components';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <StatusBar backgroundColor='#F9CA24' barStyle='dark-content'/>
+        <Header/>
         <ListContainer/>
       </View>
     );
---
+++ components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Directions } from 'react-native-gesture-handler';
+
+const Header = () => (
+    <View style={styles.container}>
+        <Image source={{uri: 'https://s3.ap-northeast-2.amazonaws.com/selfly.dilrong.com/system/LOGO.png'}} style={{width: 85, height: 27, margin: 10}}/>
+    </View>
+)
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 0.07,
+        flexDirection: 'row',
+        elevation: 5,
+        backgroundColor: '#F9CA24',
+        width: '100%',
+        elevation: 5
+    }
+});
+
+export default Header;
