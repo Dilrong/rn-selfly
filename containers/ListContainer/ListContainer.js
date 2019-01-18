481b716def3b6d1df64adb2b939a101bcfb6ec3f
hakseong Lee <wet932@naver.com>
2019-01-18 16:00:06 +0900

    update network update cardview api asnyc

--- containers/ListContainer/ListContainer.js
+++ containers/ListContainer/ListContainer.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import axios from 'react-native-axios';
 import { CardView } from '../../components';
 
 export default class ListContainer extends React.Component {
+  state = {
+    data: [],
+    isLoading: false
+  }
+
+  componentDidMount(){
+    this.getPostData();
+  }
+
+  getPostData = () => {
+    return axios.get('http://selfly.dilrong.com/posts')
+    .then((response) => {
+      this.setState({
+        data: response.data.rows,
+        isLoading: true
+      })
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <CardView/>
+        <ScrollView>
+          {this.state.data.map((data, index) => (
+            <CardView
+              key={index}
+              data={data}
+            />
+          ))}
+        </ScrollView>
       </View>
     );
   }
